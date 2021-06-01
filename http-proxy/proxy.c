1d663bede40b9ba6d167b84e6250a32df11044c9
Yaya <114.514@191.9>
2021-06-01 12:37:13 +0800

    keshangxie

--- http-proxy/proxy.c
+++ http-proxy/proxy.c
@@ -1,5 +1,7 @@
 #include "proxy_parse.h"
 
+#include <asm-generic/errno-base.h>
+// #include <cstddef>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <sys/types.h>
@@ -20,18 +22,210 @@
 
 // #define BUFFER_SIZE 1<<15
 
-const int BUFFER_SIZE = 1 << 16;
+const int BUFFER_SIZE = 1 << 14;
 
 const int maximum_process_count = 1 << 2;
 
-int available;
+const int bind_port = 11451;
+
+// package rio begin
+
+typedef struct
+{
+	int fd;
+	int cnt;
+	char *bufptr;
+	char buf[BUFFER_SIZE];
+} rio;
+
+/* 初始化鲁棒io结构体 */
+void rio_init(rio *R, int fd)
+{
+	R->fd = fd;
+	R->cnt = 0;
+	R->bufptr = R->buf;
+}
+
+/* 从fd中连续读入n个字节 */
+ssize_t rio_read_n(int fd, void *buf, size_t n)
+{
+	ssize_t nread;
+	size_t nleft = n;
+	char *bp = (char *)buf;
+	while (nleft > 0)
+	{
+		if ((nread = read(fd, bp, nleft)) < 0)
+		{
+			if (errno == EINTR)
+				nread = 0;
+			else
+				return -1;
+		}
+		else if (nread == 0)
+			break;
+		nleft -= nread;
+		bp += nread;
+	}
+	return n - nleft;
+}
+
+/* 向fd中连续写入n个字节 */
+ssize_t rio_write_n(int fd, void *buf, size_t n)
+{
+	ssize_t nwrite;
+	size_t nleft = n;
+	char *bp = (char *)buf;
+	while (nleft > 0)
+	{
+		if ((nwrite = write(fd, bp, nleft)) <= 0)
+		{
+			if (errno == EINTR)
+				nwrite = 0;
+			else
+				return -1;
+		}
+		nleft -= nwrite;
+		bp += nwrite;
+	}
+	return n;
+}
+
+/* 利用rio结构缓冲，减少调用read次数的read实现 */
+ssize_t rio_read(rio *rp, void *buf, size_t n)
+{
+	ssize_t cnt;
+	while (rp->cnt <= 0)
+	{
+		rp->cnt = read(rp->fd, rp->buf, sizeof(rp->buf));
+		if (rp->cnt < 0)
+		{
+			if (errno != EINTR)
+				return -1;
+		}
+		else if (rp->cnt == 0)
+			return 0;
+		else
+			rp->bufptr = rp->buf;
+	}
+	cnt = n;
+	if (rp->cnt < n)
+		cnt = rp->cnt;
+	memcpy(buf, rp->bufptr, cnt);
+	rp->cnt -= cnt;
+	rp->bufptr += cnt;
+	return cnt;
+}
+
+/* 从fd中读入行 */
+ssize_t rio_buffered_readline(rio *rp, void *buf, size_t n)
+{
+	size_t i, ret;
+	char c;
+	char *bp = (char *)buf;
+	// 为'\0'预留位置
+	for (i = 1; i < n; ++i)
+	{
+		if ((ret = rio_read(rp, &c, 1)) == 1)
+		{
+			*bp++ = c;
+			if (c == '\n')
+				break;
+		}
+		else if (ret == 0)
+		{
+			if (i == 1)
+				return 0;
+			else
+				break;
+		}
+		else
+			return -1;
+	}
+	*bp = '\0';
+	return i;
+}
+
+ssize_t rio_buffered_read_n(rio *rp, void *buf, size_t n)
+{
+	ssize_t nread;
+	size_t nleft = n;
+	char *bp = (char *)buf;
+	while (nleft > 0)
+	{
+		if ((nread = rio_read(rp, bp, nleft)) < 0)
+		{
+			if (errno == EINTR)
+				nread = 0;
+			else
+				return -1;
+		}
+		else if (nread == 0)
+			break;
+		nleft -= nread;
+		bp += nread;
+	}
+	return n - nleft;
+}
+
+// package rio end
+
+
+
+void parse_uri(const char raw_uri[], char protocol[], char host[], char path[], int *port)
+{
+	char ato[BUFFER_SIZE];
+
+	*port = 80;	 // 默认设80
+	path = "\0"; // 默认设空
+	protocol = "\0";
+
+	int match_cnt = sscanf(raw_uri, "%[htps]://%s", protocol, ato);
+
+	// 没有http(s)前缀
+	if (match_cnt == 0)
+	{
+		sscanf(raw_uri, "%s", ato);
+	}
+	else
+	{
+		if (strcmp(protocol, "http") == 0)
+			*port = 80;
+		else if (strcmp(protocol, "https") == 0)
+			*port = 443;
+	}
+
+	match_cnt = sscanf(ato, "%[^/:?]:%d%s", host, port, path);
+
+	// 没有端口号
+	if (match_cnt == 1)
+	{
+		sscanf(ato, "%*[^/:?]%s", path);
+	}
+}
 
 void handle_inbound(int socket_fd)
 {
 	char buf[BUFFER_SIZE];
-	int ctr = read(socket_fd, buf, BUFFER_SIZE);
-	buf[ctr] = '\0';
-	puts(buf);
+	char method[BUFFER_SIZE];
+	char uri[BUFFER_SIZE];
+	char version[BUFFER_SIZE];
+
+	rio R;
+	ssize_t len = 0;
+	int resplen = 0;
+
+	rio_init(&R, socket_fd);
+	rio_buffered_readline(&R, buf, BUFFER_SIZE);
+
+	sscanf(buf, "%s %s %s", method, uri, version);
+
+	char host[BUFFER_SIZE];
+	char protocol[BUFFER_SIZE];
+	char path[BUFFER_SIZE];
+	int port;
+	parse_uri(uri, protocol, host, path, &port);
+
+	struct hostent *H = gethostbyname(host);
 }
 
 int main(int argc, char *argv[])
@@ -50,7 +244,7 @@ int main(int argc, char *argv[])
 	}
 
 	from.sin_family = AF_INET;
-	from.sin_port = htons(11451);
+	from.sin_port = htons(bind_port);
 	from.sin_addr.s_addr = htonl(INADDR_ANY);
 
 	bzero(&(from.sin_zero), 8);
@@ -61,7 +255,7 @@ int main(int argc, char *argv[])
 	}
 
 	listen(sockfd, 1919810);
-	puts("监听11451中");
+	printf("监听%d中", bind_port);
 
 	int T = 1919810;
 
@@ -89,7 +283,7 @@ int main(int argc, char *argv[])
 				{
 					int pctr;
 					sem_getvalue(available_conn, &pctr);
-					fprintf(stderr, "\033[32m 已经使用%d个进程 \033[0m \n", maximum_process_count-pctr);
+					fprintf(stderr, "\033[32m 已经使用%d个进程 \033[0m \n", maximum_process_count - pctr);
 					handle_inbound(newfd);
 					sem_post(available_conn);
 				}
